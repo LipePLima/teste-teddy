1af7bca18d7810d79463a98908abd895ae976b39
Felipe Lima <felipe.lima0160@gmail.com>
2024-01-04 19:17:30 -0300

    feat: dados de login sendo inseridos no cookie e sendo preenchidos automaticamente se possuir dados no cookie

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 import { User } from '../../types/user'
 
@@ -8,14 +9,30 @@ import { User } from '../../types/user'
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public emptyFields = false
   public user: User  = {
     user: '',
     password: ''
   };
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private cookieService: CookieService,  
+  ) { }
+
+  ngOnInit(): void {
+    const cookieValues = {
+      cookieUSername: this.cookieService.get('username'),
+      cookiePassword: this.cookieService.get('password')
+    };
+
+    if (Object.values(cookieValues).every(value => value !== '')) {
+      this.user.user     = cookieValues.cookieUSername;
+      this.user.password = cookieValues.cookiePassword;
+
+    } 
+  }
 
   public login(user: string, password: string): void {
     if (user && password) {
@@ -28,4 +45,8 @@ export class LoginComponent {
     }
   }
 
+  public saveData(): void {
+      this.cookieService.set('username', this.user.user);
+      this.cookieService.set('password', this.user.password);
+  }
 }
